c65dc882862c238f054bc134e4199acd0b4d675d
Sam Batra <s.batra0609@gmail.com>
2026-07-18 20:54:55 -0400

    Remove profile and saved mock fallbacks

--- app/(student)/saved/page.tsx
+++ app/(student)/saved/page.tsx
@@ -1,8 +1,7 @@
 import { SavedItemsClient } from "@/components/student/saved-items-client";
 import { getClubs, getEvents } from "@/lib/firebase/public-data";
-import { currentStudent } from "@/lib/mock-data";
 
 export default async function SavedPage() {
   const [clubs, events] = await Promise.all([getClubs(), getEvents()]);
-  return <SavedItemsClient fallbackStudent={currentStudent} clubs={clubs} events={events} />;
+  return <SavedItemsClient clubs={clubs} events={events} />;
 }
